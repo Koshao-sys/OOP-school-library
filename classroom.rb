5029454cedeb14bc8d0b07dd28bd7c0fea717634
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-05-20 11:06:51 +0300

    set up associations

---
+++ classroom.rb
@@ -0,0 +1,14 @@
+class Classroom
+    attr_accessor :label, :students
+  
+    def initialize(label)
+      @label = label
+      @students = []
+    end
+  
+    # Define function to add student to Classroom students array.
+    def add_student(student)
+      @students << student
+      student.classroom = self 
+    end
+  end
