6642c1bca3d2aab9ba9a57341828ffde49cc9940
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-05-20 12:53:03 +0300

    add a basic ui

---
+++ app.rb
@@ -0,0 +1,60 @@
+require_relative 'teacher'
+require_relative 'student'
+require_relative 'book'
+require_relative 'rental'
+
+class App
+  def initialize
+    @books = []
+    @peoples = []
+  end
+
+  def list_all_books
+    @books.each { |book| puts "Title:- #{book.title}   Author:- #{book.author} \n" }
+  end
+
+  def list_all_peoples
+    @peoples.each do |people|
+      puts "[#{people.class.name}] Name:- #{people.name} ID:- #{people.id} Age:- #{people.age} \n"
+    end
+  end
+
+  def create_teacher(age, specialization, name, parent_permission)
+    @peoples << Teacher.new(age, specialization, name: name, parent_permission: parent_permission)
+  end
+
+  def create_student(age, name, parent_permission)
+    @peoples << Student.new(age, name: name, parent_permission: parent_permission)
+  end
+
+  def create_book
+    puts 'Title:-'
+    title = gets.chomp
+    puts 'Author:-'
+    author = gets.chomp
+    @books << Book.new(title, author)
+  end
+
+  def create_rental
+    puts 'Select a book from the following list by number'
+    @books.each.with_index { |book, idx| puts "#{idx}) Book #{book.title} by #{book.author}" }
+    book_index = gets.chomp.to_i
+    puts 'Select a person from the following list by number(not ID)'
+    @peoples.each.with_index do |person, idx|
+      puts "#{idx}) [#{person.class.name}] Name #{person.name}, ID #{person.id}, Age #{person.age}"
+    end
+    person_index = gets.chomp.to_i
+    puts 'Date:-'
+    date = gets.chomp
+    Rental.new(@books[book_index], @peoples[person_index], date)
+  end
+
+  def list_person_rentals(person_id)
+    @peoples.each do |person|
+      if person.id == person_id
+        puts 'Rentals:-'
+        person.rentals.each { |rental| puts "Date #{rental.date}, Book #{rental.book.title} by #{rental.book.author}" }
+      end
+    end
+  end
+end
---
+++ main.rb
@@ -0,0 +1,77 @@
+require_relative 'app'
+
+def list_option
+  puts ''
+  puts '1 - List all books.'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person id'
+  puts '7 - Exit'
+end
+
+# def create_student
+
+def create_person(app)
+  puts 'Do you want to create a student(1) or a teacher(2)? [Input the number]:-'
+  option = gets.chomp
+  parent_permission = true
+  puts 'age:-'
+  age = gets.chomp.to_i
+  puts 'name:-'
+  name = gets.chomp
+  puts 'Has parent permission?[Y/N]'
+  val = gets.chomp
+  parent_permission = false if %w[N n NO no No].include?(val)
+
+  case option.to_i
+  when 1
+    app.create_student(age, name, parent_permission)
+  when 2
+    puts 'specialization:-'
+    specialization = gets.chomp
+    app.create_teacher(age, specialization, name, parent_permission)
+  else
+    puts 'Invalid input ):'
+  end
+end
+
+def handel_option(option, app)
+  case option
+  when 3
+    create_person(app)
+  when 4
+    app.create_book
+  when 5
+    app.create_rental
+  when 6
+    puts 'Enter person ID:-'
+    id = gets.chomp.to_i
+    app.list_person_rentals(id)
+  when 7
+    puts 'Thanks :)...............'
+    return false
+  else
+    puts 'Invalid number :('
+  end
+  true
+end
+
+def main
+  status = true
+  app = App.new
+  while status
+    list_option
+    option = gets.chomp.to_i
+    if option == 1
+      app.list_all_books
+    elsif option == 2
+      app.list_all_peoples
+    else
+      status = handel_option(option, app)
+    end
+  end
+end
+
+main
