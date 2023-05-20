9a418f1c5efdeab6500194aca79f56e2aeb05e12
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-05-20 12:37:28 +0300

    Fix linter errors

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, classroom, name = 'unknown', parent_permission: true)
+  def initialize(age, _classroom, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
   end
 
