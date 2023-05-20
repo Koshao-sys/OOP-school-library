5b9796ea0ce051c20e2e68c159dc0671194d9dc0
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-05-20 11:19:59 +0300

    Fix linter errors

--- classroom.rb
+++ classroom.rb
@@ -1,14 +1,14 @@
 class Classroom
-    attr_accessor :label, :students
-  
-    def initialize(label)
-      @label = label
-      @students = []
-    end
-  
-    # Define function to add student to Classroom students array.
-    def add_student(student)
-      @students << student
-      student.classroom = self 
-    end
+  attr_accessor :label, :students
+
+  def initialize(label)
+    @label = label
+    @students = []
   end
+
+  # Define function to add student to Classroom students array.
+  def add_student(student)
+    @students << student
+    student.classroom = self
+  end
+end
