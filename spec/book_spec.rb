b50daa43158eb99ffbb4542755234c672d83cf0f
Adholah96 <adholah96@gmail.com>
2023-05-27 03:08:30 +0300

    Update book and decorator tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -3,7 +3,7 @@ require_relative '../book'
 RSpec.describe Book do
   describe '#add_rental' do
     it 'adds a rental to the book' do
-      book = Book.new('Game Of Thrones', 'Billy Ocean')
+      book = Book.new('Dark throne', 'Kevin Ocean')
       person = double('person')
       date = '2023-05-24'
       expect(Rental).to receive(:new).with(book, person, date)
--- spec/capitalize_decorator_spec.rb
+++ spec/capitalize_decorator_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../capitalize_decorator'
 
 RSpec.describe CapitalizeDecorator do
-  let(:nameable) { double('Nameable', correct_name: 'john doe') }
+  let(:nameable) { double('Nameable', correct_name: 'Kevin doe') }
   subject(:decorator) { described_class.new(nameable) }
 
   describe '#correct_name' do
